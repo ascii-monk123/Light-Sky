8e2a82891bff37a2051044e664a5c3db40b7d7c7
Aahan Singh Charak <aahancharak@gmail.com>
2020-12-15 22:17:37 +0530

    Fixed the date issue

--- src/Helpers/helpers.js
+++ src/Helpers/helpers.js
@@ -6,20 +6,20 @@ const kelvinToCelsius = (temp) => (temp - 273.15).toFixed(0);
 
 const findTime = (unixNum) => {
   let date = new Date(unixNum * 1000);
-  let meridian = '';
+  let meridian = "";
   let hours = parseInt(date.getHours());
   if (hours >= 0 && hours < 12) {
-    meridian = 'am';
+    meridian = "am";
   } else {
-    meridian = 'pm';
+    meridian = "pm";
     hours = hours - 12;
   }
   let minutes = parseInt(date.getMinutes());
   if (minutes === 60) {
-    minutes = '00';
+    minutes = "00";
   }
   if (hours === 24 || hours === 0) {
-    hours = '00';
+    hours = "00";
   }
   return `${hours}:${minutes} ${meridian}`;
 };
@@ -27,32 +27,34 @@ const findTime = (unixNum) => {
 const findDay = (unixNum) => {
   let date = new Date(unixNum * 1000);
   const days = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
   ];
   return days[date.getDay()];
 };
 const findDate = (unixNum) => {
   let date = new Date(unixNum * 1000);
+  console.log(date.getMonth());
   let months = [
-    'Jan',
-    'Feb',
-    'Apr',
-    'May',
-    'June',
-    'July',
-    'Aug',
-    'Sept',
-    'Oct',
-    'Nov',
-    'Dec',
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
   ];
-  return months[date.getMonth()] + ' ' + date.getDate();
+  return months[date.getMonth()] + " " + date.getDate();
 };
 
 export { capitalize, kelvinToCelsius, findTime, findDay, findDate };
